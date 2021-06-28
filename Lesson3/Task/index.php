1185e01301748f88e4b166ed611666a467fdac2e
nikolozi2001 <34250988+nikolozi2001@users.noreply.github.com>
2021-06-28 13:31:58 +0400

    updated first task

--- Lesson3/Task/index.php
+++ Lesson3/Task/index.php
@@ -9,11 +9,11 @@ require('./helpers.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:url" content="https://mysite/myarticle" />
+    <meta property="og:url" content="#" />
     <meta property="og:type" content="article" />
     <meta property="og:title" content="My Article Title" />
-    <meta property="og:description" content="My artcle short description" />
-    <meta property="og:image" content="https://mysite/my-article-image.jpg" />
+    <meta property="og:description" content="Lesson3 - PHP" />
+    <meta property="og:image" content="" />
     <title>TASK 3</title>
     <link rel="stylesheet" href="style.css">
 
@@ -22,33 +22,23 @@ require('./helpers.php');
 <body class="main">
 
     <?php $users = getUsersInfoFromFile();  ?>
-    
+
+
 
     <?php foreach ($users as $user) : ?>
-        <!-- <h3>
-            <?= $user['name'] ?>
-            <br>
-            <?= $user['year'] ?>
-        </h3> -->
-        
-        <h3>
-            <?php 
-                if($user['year'] > 1700){
-                    echo $user['name'];
-                    echo '<br>';
-                    echo $user['year'];
-                }
-                
-            ?>        
-        </h3>
-
+        <?php if ($user['year'] > 1700) : ?>
+            <h3>
+                <?= $user['name'] ?>
+                <br>
+                <?= $user['year'] ?>
+            </h3>
+        <?php endif; ?>
     <?php endforeach; ?>
-    
 
 
 
     <h2>Add new information in database</h2>
-    
+
     <form action="add-info.php" method="POST" enctype="multipart/form-data">
         <input type="text" placeholder="A historical event" name="name">
         <br>
@@ -56,7 +46,7 @@ require('./helpers.php');
         <input type="number" placeholder="year" name="year">
         <br>
 
-        
+
 
         <button class="click">send</button>
     </form>
